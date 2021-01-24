06d8870e277f2608aa6052d45d02adc5b2433f9e
Patrick Teulings <patrickteulings@gmail.com>
2021-01-24 21:37:42 +0100

    excluding _redirects file

---
+++ vue.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  // ...other vue-cli plugin options...
+  pwa: {
+    name: 'Gratitude',
+    themeColor: '#0F343C',
+    msTileColor: '#0F343C',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: '#0F343C',
+    manifestOptions: {
+      name: 'Gratitude',
+      shortName: 'Grattitude',
+      startUrl: '.',
+      display: 'standalone',
+      themeColor: '182C34',
+      backgroundColor: '#0F343C'
+    },
+    workboxPluginMode: 'InjectManifest',
+    workboxOptions: {
+      swSrc: 'public/service-worker.js',
+      exclude: 'public/_redirects'
+    },
+    manifestCrossorigin: 'anonymous'
+  },
+
+  css: {
+    extract: true,
+    sourceMap: true
+  }
+}
